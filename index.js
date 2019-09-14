def9137e12a874f43af359c583ed95b64737edab
drewsef-916 <drewm541@gmail.com>
2019-09-14 10:11:48 -0700

    added config var

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 
+app.get(`/`, (req, res) => {
+    res.send(`API root`)
+})
+
 app.get(`/recipes`, async (req, res) => {
     try {
         const recipes = await axios.get(`https://api.mlab.com/api/1/databases/wfddev/collections/recipes?apiKey=${process.env.MLAB_SECRET}`)
