9aa36ff9ae671bd43fd7a8bbc5681fecb1a74aea
Drew Moore <drewmoore@US-SAC-AL-H2103.local>
2019-09-25 09:13:47 -0700

    debugging

--- index.js
+++ index.js
@@ -48,19 +48,18 @@ app.get(`/recipe/:id`, async (req, res) => {
 })
 
 app.post('/add-recipe', async (req, res) => {
-    res.send(req)
-    // try {
-    //   console.dir(req)
-    //   await axios.post(`https://api.mlab.com/api/1/databases/wfddev/collections/recipes?apiKey=${process.env.MLAB_SECRET}`, {
-    //     id: req.body.id,
-    //     name: req.body.name,
-    //     ingredients: req.body.ingredients,
-    //     directions: req.body.directions
-    //   })
-    //   res.send('Success!')
-    // } catch(err) {
-    //   console.log(err)
-    // }
+    try {
+      console.dir(req)
+      await axios.post(`https://api.mlab.com/api/1/databases/wfddev/collections/recipes?apiKey=${process.env.MLAB_SECRET}`, {
+        id: req.body.id,
+        name: req.body.name,
+        ingredients: req.body.ingredients,
+        directions: req.body.directions
+      })
+      res.send('Success!')
+    } catch(err) {
+      console.log(err)
+    }
   })
 
 //   app.delete('/delete-recipe', async (req, res) => {
